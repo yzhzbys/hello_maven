e81ab81bc704b1fee6e3ad6075a1df5f38875aa3
yzhzbys <1124465491@qq.com>
2021-01-19 16:37:21 +0800

    idea hellomaven

---
+++ src/main/java/com/yuyu/maven/Hello.java
@@ -0,0 +1,7 @@
+package com.yuyu.maven;
+
+public class Hello {
+    public String sayHello(String name) {
+        return "hello"+name+"!";
+    }
+}
---
+++ src/test/java/com/yuyu/maven/HelloTest.java
@@ -0,0 +1,12 @@
+package com.yuyu.maven;
+
+import org.junit.Test;
+
+public class HelloTest {
+    @Test
+    public void testHello(){
+        Hello hello=new Hello();
+        String maven = hello.sayHello("Maven");
+        System.out.println(maven);
+    }
+}
